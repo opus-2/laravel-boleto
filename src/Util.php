2cc4e256df057d3bdaa0c817c546e49d1c63335b
opus2 <24445823+opus-2@users.noreply.github.com>
2025-03-06 01:07:49 -0300

    Corrige fator de vencimento.

--- src/Util.php
+++ src/Util.php
@@ -500,7 +500,12 @@ final class Util
     public static function fatorVencimento($date, $format = 'Y-m-d')
     {
         $date = ($date instanceof Carbon) ? $date : Carbon::createFromFormat($format, $date)->setTime(0, 0, 0);
-        return (new Carbon('1997-10-07'))->diffInDays($date);
+        $fator = (new Carbon('1997-10-07'))->diffInDays($date);
+        $limit = $fator % 9000;
+        if ($limit >= 1000) {
+            return $limit;
+        }
+        return $limit + 9000;
     }
 
     /**
